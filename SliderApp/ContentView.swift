4c6d859d237b1864dd59d896f382917fb626e38d
ceratophrysgerogero <ceratophrysgerogero@gmail.com>
2020-08-07 10:17:46 +0900

    init

---
+++ SliderApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SliderApp
+//
+//  Created by 岩崎瑛佑 on 2020/08/07.
+//  Copyright © 2020 岩崎瑛佑. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
